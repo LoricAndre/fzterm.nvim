f39250f09bcdc889b9e03dde6e39de678cdeec1f
Loric André <loric@localhost.localdomain>
2020-11-02 01:25:51 +0100

    Add support for multiple files

--- lua/fzterm/init.lua
+++ lua/fzterm/init.lua
@@ -45,7 +45,10 @@ function M.fzterm(pre_cmd, post_cmd, matcher, internal)
   end
   api.nvim_command(":term " .. pre_cmd .. " | ".. matcher .. post_cmd .. " > /tmp/fzterm")
   api.nvim_command(":start")
-  local on_close = ":au BufEnter * ++once let f = readfile('/tmp/fzterm') | if !empty(f) | execute 'edit' f[0] | endif"
+  local on_close = ":au BufEnter * ++once let f = readfile('/tmp/fzterm') | "
+  on_close = on_close .. "if !empty(f) | "
+  on_close = on_close .. "for l in f execute 'edit' f[0]"
+  on_close = on_close .. " | endif"
   api.nvim_command(on_close)
 end
 
